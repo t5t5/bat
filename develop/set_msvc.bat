fe8f3e434e8e8bf37b5d341c4e53c90257a29e44
Evgeniy Padalko <t5t5@inbox.ru>
2018-09-21 11:26:34 +0500

    Обновлен поиск компилятора Microsoft Visual C++. Добавлен поиск MSVC 2017.

--- develop/set_msvc.bat
+++ develop/set_msvc.bat
@@ -4,25 +4,43 @@
 ::
 :: Если компилятор будет найден, будут установленны соответствующие переменные среды.
 :: Если компилятор не будет найден, вывод соответсвующей информации и выход из cmd.
+::
+:: Note: Пути к VisuaStudio должны быть прописаны в %~n0.paths.
 
-if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles%\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles%\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %1
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"  %1
-) else if exist "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
-    call "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" %1
-) else (
-    echo Error: Visual Studio not found!
+if "%~1" == "" (
+    echo Error: Platform not specified. [%~f0]
     exit 1
 )
+
+setlocal EnableDelayedExpansion
+set APP=vcvarsall.bat
+
+for /f "eol=# tokens=*" %%a in (%~dpn0.paths) do (
+    call :check_and_prepare_app "%%a\%APP%"
+    if !ERRORLEVEL! == 0 ( goto :end )
+)
+echo Error: Visual studio not found. [%~f0]
+echo        Append msvc path to file: [%~dpn0.paths]
+exit 1
+
+:check_and_prepare_app
+:: Проверка наличия файла по определенному пути и подготовка для запуска.
+:: %%1 - Полный путь и имя файла для проверки.
+::
+:: Выход:
+:: ERRORLEVEL = 0 найден
+:: APP        = полный путь к файлу
+::
+:: ERRORLEVEL = 1 не найден
+if exist "%~1" (
+    set APP="%~1"
+    exit /b 0
+)
+exit /b 1
+
+:end
+endlocal&set APP=%APP%
+call %APP% %1
+set APP=
 exit /b 0
+
