40346cda9d065b8075f76b235b65ea9b8a0e187f
Evgeniy Padalko <t5t5@inbox.ru>
2018-11-05 19:08:25 +0500

    Обновлен поиск компилятора Microsoft Visual C++.
    -- Выбор платформы с зависимости от PROCESSOR_ARCHITECTURE;
    -- Убрана MSVC 2008.

--- develop/set_msvc.bat
+++ develop/set_msvc.bat
@@ -13,6 +13,13 @@ if "%~1" == "" (
 )
 
 setlocal EnableDelayedExpansion
+
+call :detect_target_platform_%PROCESSOR_ARCHITECTURE%_%~1 TARGET_PLATFORM
+if "%TARGET_PLATFORM%" == "" (
+     echo Error. Unknown visual studio platform.
+     exit 1
+)
+
 set APP=vcvarsall.bat
 
 for /f "eol=# tokens=*" %%a in (%~dpn0.paths) do (
@@ -38,9 +45,35 @@ if exist "%~1" (
 )
 exit /b 1
 
+:detect_target_platform_AMD64_x86:
+set %~1=amd64_x86
+exit /b 0
+
+:detect_target_platform_AMD64_x32:
+set %~1=amd64_x86
+exit /b 0
+
+:detect_target_platform_AMD64_x64:
+set %~1=amd64
+exit /b 0
+
+:detect_target_platform_x86_x86:
+set %~1=x86
+exit /b 0
+
+:detect_target_platform_x86_x32:
+set %~1=x86
+exit /b 0
+
+:detect_target_platform_x86_x64:
+set %~1=x86_amd64
+exit /b 0
+
+
 :end
-endlocal&set APP=%APP%
-call %APP% %1
+endlocal&set APP=%APP%&set TARGET_PLATFORM=%TARGET_PLATFORM%
+call %APP% %TARGET_PLATFORM%
 set APP=
+set TARGET_PLATFORM=
 exit /b 0
 
