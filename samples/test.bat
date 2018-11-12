cdfa9e2cde8022de3d4feec23d7833ba29e41ce6
Evgeniy Padalko <t5t5@inbox.ru>
2018-11-12 23:57:30 +0500

    Разбор аргументов командной строки по таблице. Начало.

---
+++ samples/test.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+if "%~1" == "-m" (
+    call :%~2
+    if !ERRORLEVEL! == 1 (
+        echo Error. Method not found {%~2}. [%~f0]
+        exit 1
+    )
+) else (
+    call parse_arguments.bat "%~0" "args.txt" %*
+) 
+
+endlocal
+exit /b 0
+
+:: ------------------------------------------------------------------------------------------------
+:usage
+echo Usage: %~nx0 [options]
+
+exit /b 0
