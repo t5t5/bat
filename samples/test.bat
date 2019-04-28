acfa5f9250070023aa65c5804e7182dfed849c90
Evgeniy Padalko <t5t5@inbox.ru>
2019-04-28 20:27:41 +0500

    Разбор аргументов командной строки по таблице. Продолжение.

--- samples/test.bat
+++ samples/test.bat
@@ -8,9 +8,13 @@ if "%~1" == "-m" (
         echo Error. Method not found {%~2}. [%~f0]
         exit 1
     )
+    exit 0
 ) else (
     call parse_arguments.bat "%~0" "args.txt" %*
 ) 
+
+echo ----------------------------
+set 
 
 endlocal
 exit /b 0
